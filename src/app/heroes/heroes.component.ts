89ae6dbcc1556a645593d32ede83a664fc86ebfe
samir.ayoub <samir.ayoub@SPOD3403.local>
2018-01-18 13:30:37 -0200

    adicionando rota parametrizada

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,12 +13,6 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
   
   heroes: Hero[];
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
 
   constructor(private heroService: HeroService) { }
 
@@ -36,7 +30,5 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
-
-
 
 }
